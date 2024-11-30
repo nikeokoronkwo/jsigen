2ca5224c0caa7415c40afbb006d20a96c843083a
nikechukwu <nikechukwu@googlemail.com>
2024-11-30 03:21:48 +0000

    remigrated to root

--- packages/js_generator/lib/js_generator.dart
+++
@@ -1,9 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-import 'src/common.dart'
-if (dart.library.js) 'src/js.dart';
-
-// TODO: Export any libraries intended for clients of this package.
--- packages/js_generator/test/js_generator_test.dart
+++
@@ -1,6 +0,0 @@
-import 'package:js_generator/js_generator.dart';
-import 'package:test/test.dart';
-
-void main() {
-  
-}
